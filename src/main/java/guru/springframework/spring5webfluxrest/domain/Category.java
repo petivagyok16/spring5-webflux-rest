dc4df1a4252fc7d4582865c0f14615b7ccd6d91e
Peter P <peter.palocz91@gmail.com>
2018-04-23 13:09:14 +0200

    fix(Domain): add NoArgsContructor

--- src/main/java/guru/springframework/spring5webfluxrest/domain/Category.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Category.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
 	@Id
--- src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class Vendor {
 
 	@Id
