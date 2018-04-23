95b73b307639b677f8d9e908838e668eff0a7ea3
Peter P <peter.palocz91@gmail.com>
2018-04-23 12:29:17 +0200

    feat(Vendor): add vendor class & repository

---
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring5webfluxrest.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@AllArgsConstructor
+@Builder
+public class Vendor {
+
+	@Id
+	public String id;
+
+	public String firstName;
+	public String lastName;
+}
